1cc190da105ac262512896ce9193fbb52d564425
Gustav Lind <gustavlind@hotmail.com>
2013-10-01 11:41:39 +0200

    Lagt till kod  i Board. Ändrat propertys i Input

--- TicTacToe/Board.cs
+++ TicTacToe/Board.cs
@@ -1,30 +1,56 @@
 ﻿using System;
 
-
-public class Board
+namespace TicTacToe
 {
-    public Board()
+    public class Board
     {
+        private Pointer[,] p = new Pointer[Console.WindowHeight, Console.WindowWidth];
 
-    }
-
-    public void BoardGraphics()
-    {
-        for (int x = 0; x < 3; x++)
+        public Board()
         {
-            for (int y = 0; y < 3; y++)
+            FillBoard();
+        }
+
+        public void BoardGraphics()
+        {
+            for (int x = 0; x < 3; x++)
             {
-                draw(x * 5, y * 3);
+                for (int y = 0; y < 3; y++)
+                {
+                    draw(x * 5, y * 3);
+                }
             }
         }
-    }
-    public void draw(int x, int y)
-    {
-        Console.SetCursorPosition(x, y);
-        Console.Write("┌───┐");
-        Console.SetCursorPosition(x, y + 1);
-        Console.Write("│   │");
-        Console.SetCursorPosition(x, y + 2);
-        Console.Write("└───┘");
+        public void draw(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("┌───┐");
+            Console.SetCursorPosition(x, y + 1);
+            Console.Write("│   │");
+            Console.SetCursorPosition(x, y + 2);
+            Console.Write("└───┘");
+        }
+        public void FillBoard()
+        {
+            for (int x = 0; x < Console.WindowWidth; x++)
+            {
+                for (int y = 0; y < Console.WindowHeight; y++)
+                {
+                    p[x, y] = new Pointer();
+                }
+
+            }
+        }
+
+
+        public void setValue(int value, int x, int y)
+        {
+            p[x, y].setValue(value);
+        }
+
+        public int GetValue(int x, int y)
+        {
+            return p[x, y].GetValue();
+        }
     }
 }
--- TicTacToe/Input.cs
+++ TicTacToe/Input.cs
@@ -8,5 +8,55 @@ namespace TicTacToe
 {
     class Input
     {
+        private Cursor cursor;
+        private Board board;
+        private Render render;
+        private bool turn { get; set; }
+
+        public Input()
+        {
+            board = new Board();
+            cursor = new Cursor();
+            render = new Render();
+            turn = true;
+
+        }
+
+        public void Move()
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.UpArrow)     // Uppil flyttar markören ett steg upp
+                    cursor.MoveUp();
+                else if (key.Key == ConsoleKey.DownArrow)   // Nerpil flyttar markören ett steg ner 
+                    cursor.MoveDown();
+                else if (key.Key == ConsoleKey.LeftArrow)   // Vänsterpil flyttar markören ett steg åt vänster
+                    cursor.MoveLeft();
+                else if (key.Key == ConsoleKey.RightArrow)  // Högerpil flyttar markören ett steg åt höger
+                    cursor.MoveRight();
+                if (key.Key == ConsoleKey.Spacebar)
+                {
+
+                    if (board.GetValue(cursor.mXpos, cursor.mYpos) == 0)
+                    {
+                        if (turn)
+                        {
+                            board.setValue(1, cursor.mXpos, cursor.mYpos);
+                            turn = !turn;
+                        }
+                        else
+                        {
+                            board.setValue(2, cursor.mXpos, cursor.mYpos);
+                            turn = !turn;
+                        }
+                    }
+
+                }
+
+            }
+
+            render.draw(board);
+        }
     }
 }
